999475187c256294b8b0a9b3f0df7e8ca9187f02
ramazanovkuat <kuat12@yandex.ru>
2024-11-17 10:31:56 +0000

    first dvc pipeline

---
+++ scripts/evaluate.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold, cross_validate
+import joblib
+import json
+import yaml
+import os
+
+# оценка качества модели
+def evaluate_model():
+	# прочитайте файл с гиперпараметрами params.yaml
+    with open('params.yaml', 'r') as fd:
+        params = yaml.safe_load(fd)
+	# загрузите результат прошлого шага: fitted_model.pkl
+    pipeline = joblib.load(open('models/fitted_model.pkl', 'rb')) 
+	# реализуйте основную логику шага с использованием прочтённых гиперпараметров
+    data = pd.read_csv('data/initial_data.csv')
+    cv_strategy = StratifiedKFold(n_splits=params['n_splits'])
+    cv_res = cross_validate(
+        pipeline,
+        data,
+        data[params['target_col']],
+        cv=cv_strategy,
+        n_jobs=params['n_jobs'],
+        scoring=params['metrics']
+        )
+    for key, value in cv_res.items():
+        cv_res[key] = value.tolist()
+	# сохраните результата кросс-валидации в cv_res.json
+    os.makedirs('cv_results', exist_ok=True)
+    with open('cv_results/cv_res.json', 'w') as fp:
+        json.dump(cv_res, fp)
+        
+if __name__ == '__main__':
+	evaluate_model()
